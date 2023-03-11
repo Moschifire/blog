883f6e7006cfb36eefafe8602ab3754db56413cf
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-03-11 20:24:10 +0100

    all files

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import EmptyList from '../../components/common/EmptyList'
+import BlogList from '../../components/Home/BlogList'
 import Header from '../../components/Home/Header'
 import SearchBar from '../../components/Home/Searchbar'
+import { blogList } from '../../config/data'
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(blogList);
+  const [searchKey, setSearchKey] = useState("");
+
+  // Search Submit
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    handleSearchResults();
+  }
+
+  // Search for blogs by category
+  const handleSearchResults = () => {
+    const allBlogs = blogList;
+    const filteredBlogs = allBlogs.filter((blog) => blog.category.toLowerCase().includes(searchKey.toLowerCase().trim));
+
+    setBlogs(filteredBlogs);
+  };
+
+  const handleClearSearch = () => {
+    setBlogs(blogList);
+    setSearchKey("");
+  }
+
   return (
     <div>
         {/* Page Header */}
         <Header />
 
         {/* Search Bar */}
-        <SearchBar />
+        <SearchBar value={searchKey} clearSearch={handleClearSearch} formSubmit={handleSearchSubmit} handleSearchKey={(e) => setSearchKey(e.target.value)} />
 
         {/* Blog List & Empty List */}
+        {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
     </div>
   )
 }
